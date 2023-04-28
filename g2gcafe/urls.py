d50f0d4409c1c394ba2b7bfb5dbce639f1d60c72
Boierul <49337867+Boierul@users.noreply.github.com>
2023-04-28 20:00:35 +0200

    Added css styles

--- g2gcafe/urls.py
+++ g2gcafe/urls.py
@@ -1,25 +1,9 @@
-"""
-URL configuration for g2gcafe project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from catalogue.views import drink_menu, order_drink
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', drink_menu, name="drink_menu"),
-    path('order/', order_drink, name="order_drink"),
+    path('menu/', drink_menu, name="drink_menu"),
+    path('', order_drink, name="order_drink"),
 ]
